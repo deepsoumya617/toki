8662baa000c36b6a63c10050bd5f03833f7f965e
Soumyadeep Ghosh <soumyadeepghosh617@gmail.com>
2026-03-19 18:00:01 +0530

    feat(web): redesign signin form

--- apps/web/components/form/sign-in-form.tsx
+++ apps/web/components/form/sign-in-form.tsx
@@ -50,15 +50,22 @@ export function SignInForm() {
   }
 
   return (
-    <div className="mx-auto w-full max-w-sm rounded-3xl border border-stone-200 bg-stone-50 p-2">
-      <div className="relative overflow-hidden rounded-2xl border border-stone-200 bg-white px-4 pt-4 pb-5">
-        <div className="mb-2.5 flex flex-col -space-y-5">
-          <h1 className="bg-linear-to-r from-stone-600 to-stone-900 bg-clip-text text-3xl font-cooper font-medium tracking-tight text-transparent italic">
-            welcome
-          </h1>
-          <h1 className="bg-linear-to-r from-stone-600 to-stone-900 bg-clip-text text-[45px] font-cooper font-medium tracking-tight text-transparent">
-            back
-          </h1>
+    <div className="relative mx-auto w-full max-w-sm rounded-none border border-stone-200 bg-stone-50 p-2">
+      <div className="pointer-events-none absolute inset-0" aria-hidden="true">
+        <span className="absolute -top-px -left-px h-8 w-8 border-t border-l border-black" />
+        <span className="absolute -top-px -right-px h-8 w-8 border-t border-r border-black" />
+        <span className="absolute -bottom-px -left-px h-8 w-8 border-b border-l border-black" />
+        <span className="absolute -right-px -bottom-px h-8 w-8 border-r border-b border-black" />
+
+        <span className="absolute -top-1 -left-1 z-10 size-2 bg-black" />
+        <span className="absolute -top-1 -right-1 z-10 size-2 bg-black" />
+        <span className="absolute -bottom-1 -left-1 z-10 size-2 bg-black" />
+        <span className="absolute -right-1 -bottom-1 z-10 size-2 bg-black" />
+      </div>
+      <div className="relative overflow-hidden rounded-none border border-dashed border-stone-300 bg-white px-4 pt-4 pb-5">
+        <div className="flex flex-col -space-y-4 mb-2.5 font-pixel-square">
+          <h1 className="text-xl tracking-tight">welcome</h1>
+          <h1 className="text-[45px] tracking">back!</h1>
         </div>
 
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-3">
@@ -74,7 +81,7 @@ export function SignInForm() {
                 placeholder="Email"
                 disabled={signInMutation.isPending}
                 {...register('email')}
-                className="h-10 w-full rounded-lg border border-stone-200 bg-stone-100 pr-3 pl-9 text-xs text-stone-900 outline-none transition focus:border-stone-300 focus:bg-white"
+                className="h-10 w-full rounded-none border border-stone-200 bg-stone-100 pr-3 pl-9 text-xs text-stone-900 outline-none transition focus:border-stone-300 focus:bg-white"
               />
             </div>
             {errors.email && (
@@ -96,7 +103,7 @@ export function SignInForm() {
                 placeholder="Password"
                 disabled={signInMutation.isPending}
                 {...register('password')}
-                className="h-10 w-full rounded-lg border border-stone-200 bg-stone-100 pr-3 pl-9 text-xs text-stone-900 outline-none transition focus:border-stone-300 focus:bg-white"
+                className="h-10 w-full rounded-none border border-stone-200 bg-stone-100 pr-3 pl-9 text-xs text-stone-900 outline-none transition focus:border-stone-300 focus:bg-white"
               />
             </div>
             {errors.password && (
@@ -108,7 +115,7 @@ export function SignInForm() {
 
           <Button
             type="submit"
-            className="h-10 w-full cursor-pointer rounded-lg bg-[#84934A] text-sm font-medium text-white hover:bg-[#7a8a42]"
+            className="h-10 w-full cursor-pointer rounded-none font-pixel-square bg-stone-900 hover:bg-stone-800 disabled:bg-stone-500 disabled:cursor-not-allowed"
             disabled={signInMutation.isPending}
           >
             {signInMutation.isPending ? 'Signing in...' : 'Sign in'}
@@ -116,7 +123,7 @@ export function SignInForm() {
         </form>
       </div>
 
-      <p className="mt-5 mb-3 text-center text-sm text-stone-500">
+      <p className="mt-5 mb-3 text-center text-[0.8125rem] text-stone-500 font-pixel-square">
         Don&apos;t have an account?{' '}
         <Link
           href="/sign-up"
